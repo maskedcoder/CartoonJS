788465c33db0e30bad0ee7f14e38f51ec218d012
Andrew Myers <CubeyZimmey@gmail.com>
2013-07-17 15:10:59 -0500

    Wrote tests for GenericCartoonItem and CartoonImageItem (added notPixelEqual to QUnit)

--- test/resources/qunit-1.12.0.js
+++ test/resources/qunit-1.12.0.js
@@ -627,6 +627,16 @@ assert = {
 		QUnit.push(QUnit.equiv(actual, expected), actual, expected, message);
 	},
 
+	/**
+	 * @name noPixelEqual
+	 * @function
+	 */
+	notPixelEqual: function(canvas, x, y, r, g, b, a, message) {
+		var actual = Array.prototype.slice.apply(canvas.getContext("2d").getImageData(x, y, 1, 1).data),
+			expected = [r, g, b, a];
+		QUnit.push( !QUnit.equiv(actual, expected), actual, expected, message);
+	},
+
 	"throws": function( block, expected, message ) {
 		var actual,
 			expectedOutput = expected,
